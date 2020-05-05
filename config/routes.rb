df0da345d53480a157244483b4004db3d026d9fb
Rodney Moura <rodney.rms@gmail.com>
2020-05-05 20:22:17 -0300

    Config routes to tickets and api-docs with swagger

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount OpenApi::Rswag::Ui::Engine  => '/api-docs'
+  mount OpenApi::Rswag::Api::Engine => '/api-docs'
 
   root 'health_check/health_check#index', defaults: { format: :json }
+
+  namespace :v1, path: '' do
+    resource :tickets, only: %i[create], path: 'parking' do
+      put '/:id/out', action: :out
+      put '/:id/pay', action: :pay
+      get '/:plate',  action: :show
+    end
+  end
+
+  match '*any', to: 'application#not_found', via: :all
 end
